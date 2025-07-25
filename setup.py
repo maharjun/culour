9f6e6df39a469694dcb0f26e6c6397ad355fb1f1
Arjun Rao <arjun210493@gmail.com>
2025-07-25 10:15:31 +1000

    Bump version to 0.4.dev0 for development release
    - Updated version from 0.1 to 0.4.dev0 to indicate development version
    - Updated description to mention bold and underline support
    - Updated URL to point to the fork repository
    - Added keywords for better discoverability

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name='culour',
     packages=['culour'],
-    version='0.1',
-    description='Print colored strings in a curses window',
+    version='0.4.dev0',
+    description='Print colored strings in a curses window with support for bold and underline',
     author='Shoham Peller',
     author_email='shohamp@gmail.com',
-    url='https://github.com/shohamp/culour',
-    download_url='https://github.com/shohamp/culour/tarball/0.1',
-    keywords=['curses', 'color', 'colour'],
+    url='https://github.com/maharjun/culour',
+    download_url='https://github.com/maharjun/culour/tarball/0.4.dev0',
+    keywords=['curses', 'color', 'colour', 'ansi', 'bold', 'underline'],
     classifiers=[],
 )
